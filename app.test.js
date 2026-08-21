15fcf4454499a98bc0a7882089f9a0e59bf3517a
janhavi <janhavikaldate30@gmail.com>
2026-08-21 21:34:25 +0530

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,5 +1,13 @@
-javascript
 const add = require('./app.js');
+
 test('adds 2 + 3 to equal 5', () => {
-expect(add(2, 3)).toBe(5);
+    expect(add(2, 3)).toBe(5);
+});
+
+test('adds 10 + 20 to equal 30', () => {
+    expect(add(10, 20)).toBe(30);
+});
+
+test('adds negative numbers correctly', () => {
+    expect(add(-2, -3)).toBe(-5);
 });
